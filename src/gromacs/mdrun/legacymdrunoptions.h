21a1fe6b7063de5ec347a9fb0b24bdf2963c6cbc
Michele <micpel@kth.se>
2021-04-06 08:45:10 +0200

    Updated legacy options

--- src/gromacs/mdrun/legacymdrunoptions.h
+++ src/gromacs/mdrun/legacymdrunoptions.h
@@ -95,6 +95,11 @@ public:
     //! Parameters for replica-exchange simulations.
     ReplicaExchangeParameters replExParams;
 
+    /* MICHELE */
+    /* Dummy bool variable for flow_rtx
+     */
+    bool dummy_flow_rtx;
+
     //! Filename options to fill from command-line argument values.
     std::vector<t_filenm> filenames = { { { efTPR, nullptr, nullptr, ffREAD },
                                           { efTRN, "-o", nullptr, ffWRITE },
@@ -277,7 +282,7 @@ public:
 	{ "-flow_rtx",
           FALSE,
           etBOOL,
-          { nullptr },
+          { &dummy_flow_rtx },
           "Perform half-step re-tracing on atomic positions when performing flow variables binning" },
         { "-nb", FALSE, etENUM, { nbpu_opt_choices }, "Calculate non-bonded interactions on" },
         { "-nstlist",
